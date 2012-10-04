3d8f370fcf9e06ac6ef2700be7208f2aef63d53b
vadv <vadv.mkn@gmail.com>
2012-10-04 20:16:37 +0400

    fix Unlink
    see http://www.zabbix.com/documentation/1.8/api/template/massremove

--- lib/zabbixapi/template.rb
+++ lib/zabbixapi/template.rb
@@ -150,8 +150,8 @@ module Zabbix
       message = {
         'method' => 'template.massRemove',
         'params' => {
-          'hosts' => message_hosts_id,
-          'templates' => message_templates_id,
+          'hostids' => message_hosts_id,
+          'templateids' => message_templates_id,
           'force' => '1'
         }
       }
